4f1c05416a327ab2c9db143712853f2b219725b7
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2025-12-14 21:28:11 +0100

    Implement MySQL database creation script

---
+++ MySQLServer.py
@@ -0,0 +1,41 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+def create_database():
+    # Database connection configuration
+    db_config = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Ajiri19?',
+    }
+    DB_NAME = 'alx_book_store'
+    
+    cnx = None
+    try:
+        # Connect to the MySQL server
+        cnx = mysql.connector.connect(**db_config)
+        cursor = cnx.cursor()
+
+        # Execute the SQL command
+        database_creation_query = f"CREATE DATABASE IF NOT EXISTS {DB_NAME}"
+        
+        cursor.execute(database_creation_query)
+        
+        print(f"Database '{DB_NAME}' created successfully!")
+
+    except mysql.connector.Error as err:
+        # Handle connection errors
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Error: Failed to connect to DB. Something is wrong with your username or password.")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print(f"Error: Database does not exist.")
+        else:
+            print(f"Error: Failed to connect to DB. {err}")
+    finally:
+        # Handle close of the DB connection
+        if cnx and cnx.is_connected():
+            cursor.close()
+            cnx.close()
+
+if __name__ == "__main__":
+    create_database()
